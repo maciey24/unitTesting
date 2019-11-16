6a91393123a40754761308a3d8a395dc2700a09b
Maciej Ślusarz <189789@edu.p.lodz.pl>
2019-11-16 12:55:58 +0100

    test nieprzechodzący pozytywnie

--- src/test/java/com/example/demo/DzialanieTest.java
+++ src/test/java/com/example/demo/DzialanieTest.java
@@ -30,4 +30,22 @@ class DzialanieTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void dodajTest3() {
+        Integer a = 2;
+        Integer b = 2;
+        Dzialanie dzialanie = new Dzialanie();
+        Integer expected = 5;
+
+        Integer actual = dzialanie.dodaj(a, b);
+
+        assertEquals(expected, actual);
+//        jest rzucony
+//        AssertionFailedError (org.opentest4j)
+//        AssertionError (java.lang)
+//        Error (java.lang)
+//        Throwable (java.lang)
+
+    }
+
 }
