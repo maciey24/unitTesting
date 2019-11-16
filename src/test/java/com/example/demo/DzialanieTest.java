85720043c69bd3c05af79032d3e95f490d254df5
Maciej Ślusarz <189789@edu.p.lodz.pl>
2019-11-16 12:36:11 +0100

    drugi test jednostkowy dla tej samej metody

--- src/test/java/com/example/demo/DzialanieTest.java
+++ src/test/java/com/example/demo/DzialanieTest.java
@@ -7,7 +7,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class DzialanieTest {
 
     @Test
-    void dodaj() {
+    void dodajTest() {
+        int actual = new Dzialanie().dodaj(3, 6);
+        assertEquals(9, actual);
+    }
 
+    @Test
+    void dodajTest2() {
+        int actual = new Dzialanie().dodaj(-10, 3);
+        assertEquals(-7, actual);
     }
 }
