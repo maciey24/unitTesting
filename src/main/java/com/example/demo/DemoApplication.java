7537ea58df9eedf64e19b8bcfcd845bf0c308c61
Maciej Ślusarz <189789@edu.p.lodz.pl>
2019-11-16 12:30:25 +0100

    odkomentowanie run DemoApplication ze springa

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        Dzialanie dzialanie = new Dzialanie();
-        System.out.println(dzialanie.dodaj(13, 15));
-        System.out.println(dzialanie.dodaj(0, 0));
-        System.out.println(dzialanie.dodaj(-50, -10));
-//        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
