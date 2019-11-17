6afdd9fdd2f749e3557539ba151640e0ae8a3e0f
Maciej Ślusarz <189789@edu.p.lodz.pl>
2019-11-17 14:58:59 +0100

    antyprzykład 3 - kiedy nie da się mockować

--- src/main/java/com/example/demo/Dzialanie.java
+++ src/main/java/com/example/demo/Dzialanie.java
@@ -4,14 +4,13 @@ import lombok.Setter;
 
 public class Dzialanie {
 
-    Serwis1 serwis1 = new Serwis1();
-    Serwis2 serwis2 = new Serwis2();
-
     public Integer dodaj(final Integer a, final Integer b) {
         return a + b;
     }
 
     public Integer dodajLiczbyPobraneSkads() {
+        Serwis1 serwis1 = new Serwis1();
+        Serwis2 serwis2 = new Serwis2();
         Integer pobranaLiczba1 = serwis1.pobierzLiczbe();
         Integer pobranaLiczba2 = serwis2.pobierzLiczbe();
         return pobranaLiczba1 + pobranaLiczba2;
