5fec2c2dda39959011f40313f202de417ff91c54
Maciej Ślusarz <189789@edu.p.lodz.pl>
2019-11-17 15:33:47 +0100

    "zepsute" testy metody która teraz jest już prywatna - jak to naprawić...?

--- src/test/java/com/example/demo/DzialanieTest.java
+++ src/test/java/com/example/demo/DzialanieTest.java
@@ -1,53 +1,53 @@
-//package com.example.demo;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.function.Executable;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//
-//class DzialanieTest {
-//
-//    Dzialanie dzialanie;
-//
-//    @BeforeEach
-//    void beforeEach() {
-//        this.dzialanie = new Dzialanie();
-//    }
-//
-//    @Test
-//    void dodajTest() {
-//        Integer a = 3;
-//        Integer b = 6;
-//        Integer expected = 9;
-//
-//        Integer actual = dzialanie.dodaj(a, b);
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    void dodajTest2() {
-//        Integer a = -10;
-//        Integer b = 5;
-//        Integer expected = -5;
-//
-//        Integer actual = dzialanie.dodaj(a, b);
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    void dodajTest4() {
-//        Integer a = null;
-//        Integer b = 2;
-//
-//        Executable executable = () -> {
-//            dzialanie.dodaj(a, b);
-//        };
-//
-//        assertThrows(NullPointerException.class, executable);
-//    }
-//
-//}
+package com.example.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class DzialanieTest {
+
+    Dzialanie dzialanie;
+
+    @BeforeEach
+    void beforeEach() {
+        this.dzialanie = new Dzialanie();
+    }
+
+    @Test
+    void dodajTest() {
+        Integer a = 3;
+        Integer b = 6;
+        Integer expected = 9;
+
+        Integer actual = dzialanie.dodaj(a, b);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void dodajTest2() {
+        Integer a = -10;
+        Integer b = 5;
+        Integer expected = -5;
+
+        Integer actual = dzialanie.dodaj(a, b);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void dodajTest4() {
+        Integer a = null;
+        Integer b = 2;
+
+        Executable executable = () -> {
+            dzialanie.dodaj(a, b);
+        };
+
+        assertThrows(NullPointerException.class, executable);
+    }
+
+}
