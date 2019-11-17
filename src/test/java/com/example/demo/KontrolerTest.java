4f2e13d48eea8b361a1b2f91c28bf569d29939e9
Maciej Ślusarz <189789@edu.p.lodz.pl>
2019-11-18 00:38:58 +0100

    test kontrollera z mockMvc

---
+++ src/test/java/com/example/demo/KontrolerTest.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class KontrolerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void dodajTest() throws Exception {
+        final String expected = "5";
+
+        final String actual = mvc.perform(get("/dodaj")
+                .header("a", 2)
+                .header("b", 3)
+        ).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+
+        assertEquals(expected, actual);
+    }
+
+}
