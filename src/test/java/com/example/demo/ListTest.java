547f85c6f7cb489541fb3e872479286bbb418434
Maciej Ślusarz <189789@edu.p.lodz.pl>
2019-11-17 16:33:13 +0100

    antyprzykład - dlaczego nie można testować na tym samym obiekcie w wielu testach arbitralność kolejności wykonywania testów

---
+++ src/test/java/com/example/demo/ListTest.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ListTest {
+
+    List<Object> pojemnik;
+
+    @Test
+    void dodaj() {
+        pojemnik = new ArrayList<>();
+
+        pojemnik.add("abc");
+
+        assertEquals(1, pojemnik.size());
+    }
+
+    @Test
+    void usun() {
+        assertEquals(1, pojemnik.size());
+
+        pojemnik.remove("abc");
+
+        assertEquals(0, pojemnik.size());
+    }
+
+}
