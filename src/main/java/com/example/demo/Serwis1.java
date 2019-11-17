89e34d1976595215361fc9077bae79025379146e
Maciej Ślusarz <189789@edu.p.lodz.pl>
2019-11-17 15:24:11 +0100

    funkcja pobierająca dane teraz wywołuje funkcję prywatną odpowiedzialną za "logikę"

--- src/main/java/com/example/demo/Serwis1.java
+++ src/main/java/com/example/demo/Serwis1.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 public class Serwis1 {
-    public static Integer pobierzLiczbe() {
+    public Integer pobierzLiczbe() {
         //załóżmy że tutaj znajduje się bardzo złożona logika
         //dotycząca połączenia z zewnętrznym systemem
         return 10;
